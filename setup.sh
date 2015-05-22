cabd8807035d88abb824205926567f39bfd20c26
Niraj Thapaliya <niraj.thapaliya@gmail.com>
2015-05-21 23:28:11 -0400

    using dotbot as a submodule, better management

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOTFILES_LOCATION=$HOME/.dotfiles/
-
-ln -s $DOTFILES_LOCATION/vim/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_LOCATION/vim/.vim $HOME/.vim
-ln -s $DOTFILES_LOCATION/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $DOTFILES_LOCATION/tmux/.tmate.conf $HOME/.tmate.conf
