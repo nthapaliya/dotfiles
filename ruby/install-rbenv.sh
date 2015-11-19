56e32762369f6e15f50499375901db78be23e5fe
Niraj Thapaliya <niraj.thapaliya@gmail.com>
2015-11-19 14:40:25 -0500

    Script to install rbenv and some rbenv plugins

---
+++ ruby/install-rbenv.sh
@@ -0,0 +1,12 @@
+RBENV=$HOME/.rbenv
+PLUGINS=$RBENV/plugins
+
+# install rbenv
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+
+mkdir -p $PLUGINS
+
+# install plugins
+git clone https://github.com/sstephenson/ruby-build.git $PLUGINS/ruby-build
+git clone git://github.com/tpope/rbenv-ctags.git $PLUGINS/rbenv-ctags
+git clone https://github.com/rkh/rbenv-update.git $PLUGINS/rbenv-update
