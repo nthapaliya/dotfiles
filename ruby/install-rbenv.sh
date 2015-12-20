62ce9213d9ea3766f056fc809b123df3c375525a
Niraj Thapaliya <niraj.thapaliya@gmail.com>
2015-12-20 00:26:55 -0500

    Periodic updates

--- ruby/install-rbenv.sh
+++ ruby/install-rbenv.sh
@@ -10,3 +10,4 @@ mkdir -p $PLUGINS
 git clone https://github.com/sstephenson/ruby-build.git $PLUGINS/ruby-build
 git clone git://github.com/tpope/rbenv-ctags.git $PLUGINS/rbenv-ctags
 git clone https://github.com/rkh/rbenv-update.git $PLUGINS/rbenv-update
+git clone https://github.com/ianheggie/rbenv-binstubs.git $PLUGINS/rbenv-binstubs
