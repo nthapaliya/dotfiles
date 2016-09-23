650e2a7a287b21a4a99de5a7b9576946d852f552
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2016-09-22 22:21:32 -0600

    Brewfile and updates to rbenv install script

--- ruby/install-rbenv.sh
+++ ruby/install-rbenv.sh
@@ -1,13 +1,13 @@
-RBENV=$HOME/.rbenv
-PLUGINS=$RBENV/plugins
+rbenv_root="${RBENV_ROOT:-$HOME/.rbenv}"
+plugins_dir="$rbenv_root/plugins"
 
 # install rbenv
-git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+git clone https://github.com/sstephenson/rbenv.git "$rbenv_root"
 
-mkdir -p $PLUGINS
+mkdir -p "$plugins_dir"
 
 # install plugins
-git clone https://github.com/sstephenson/ruby-build.git $PLUGINS/ruby-build
-git clone git://github.com/tpope/rbenv-ctags.git $PLUGINS/rbenv-ctags
-git clone https://github.com/rkh/rbenv-update.git $PLUGINS/rbenv-update
-git clone https://github.com/ianheggie/rbenv-binstubs.git $PLUGINS/rbenv-binstubs
+git clone https://github.com/sstephenson/ruby-build.git "$plugins_dir/ruby-build"
+git clone git://github.com/tpope/rbenv-ctags.git "$plugins_dir/rbenv-ctags"
+git clone https://github.com/rkh/rbenv-update.git "$plugins_dir/rbenv-update"
+git clone https://github.com/ianheggie/rbenv-binstubs.git "$plugins_dir/rbenv-binstubs"
