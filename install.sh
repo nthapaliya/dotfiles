0913ab1d4aa157509f92ef92e0d41e2586f333f8
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2016-09-29 19:37:56 -0600

    Finalize stowing

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-stow bash
-stow git
-stow ruby
-stow vim
-stow tmux
+# Checking for binaries first
+hash stow 2>/dev/null || {
+  echo 'Error! Missing command'
+  hash brew 2>/dev/null &&
+    printf 'Run "brew install stow"' ||
+    printf 'Use your package manager to install gnu stow'
+  echo ' and run this script again'
+  exit 1
+}
+
+# Actually making the symlinks
+#
+case $1 in
+  --install|-i)
+    stow -R bash git ruby vim tmux
+    ;;
+  --uninstall|-u)
+    stow -D bash git ruby vim tmux
+    ;;
+  *)
+    echo $"Usage: $0 {--install|--uninstall}"
+    exit 1
+  esac
