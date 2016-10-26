02839919e685ef8b309f21a9d8c8b453375fad73
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2016-10-25 22:34:29 -0600

    Consolidate helper scripts to macOS-bootstrap

---
+++ README.md
@@ -0,0 +1,20 @@
+### First install ###
+
+The script is not executable, read it before running.
+It should install homebrew, the packages listed in .Brewfile, and rbenv.
+
+```bash
+bash helpers/macOS-bootstrap.bash
+```
+
+### Link dotfiles ###
+
+```
+./install
+```
+
+### Unlink dotfiles ###
+
+```
+./install -u
+```
---
+++ helpers/macOS-bootstrap.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+### README FIRST ###
+# This is deliberately NOT made executable in the hope that you
+# read this before running it.
+# The script is not tested: In fact its best to run these line by line, manually
+
+# it assumes PWD is the root of the dotfiles directory
+
+
+# Download and and install homebrew
+if ! command -v brew > /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Check if homebrew installation is ok
+if ! brew doctor > /dev/null; then
+  echo 'brew doctor has found issues, fix these and rerun script'
+  exit 1
+fi
+
+# Read brewfile and install everything (reconsider this)
+# --global expects ~/.Brewfile
+# if its not there you can specify a file with --file <filename>
+if ! brew bundle check --global &> /dev/null ; then
+  brew bundle --global
+fi
+
+# just in case its not in the brewfile
+if ! command -v stow > /dev/null; then
+  brew install stow
+fi
+
+# will probably fail for the first time because of default bashrc
+stow -R --target="$HOME" .
+
+# Download, rbenv and some useful plugins
+# probably want to change the default, which is ~/.rbenv
+if ! command -v rbenv > /dev/null ; then
+  rbenv_root="${RBENV_ROOT:-$HOME/.rbenv}"
+  plugins_dir="$rbenv_root/plugins"
+
+  # install rbenv
+  git clone https://github.com/rbenv/rbenv.git "$rbenv_root"
+
+  # Optionally, try to compile dynamic bash extension to speed up rbenv.
+  cd "$rbenv_root" && src/configure && make -C src
+
+  mkdir -p "$plugins_dir"
+
+  # install plugins
+  git clone https://github.com/rbenv/ruby-build.git "$plugins_dir/ruby-build"
+  git clone https://github.com/tpope/rbenv-ctags.git "$plugins_dir/rbenv-ctags"
+  git clone https://github.com/rkh/rbenv-update.git "$plugins_dir/rbenv-update"
+fi
+
+# TODO: zsh helpers/link-prezto
