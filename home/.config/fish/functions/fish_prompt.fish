0fd245b371f7b0faf249cc99b858caddeb35394c
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2017-08-03 13:37:41 -0600

    Edit prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,15 @@
 function fish_prompt --description 'Write out the prompt'
-	if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
+    set -l last_status $status
+    if test $last_status -ne 0
+      set prompt_last_status " [$last_status]"
+    else
+      set prompt_last_status
     end
 
-    if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
+    set -l cyan   (set_color cyan)
+    set -l green  (set_color green)
+    set -l red    (set_color red)
+    set -l normal (set_color normal)
 
-    echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+    echo -n -s "$cyan" (prompt_pwd) "$green" (__fish_vcs_prompt) "$red" "$prompt_last_status" "$normal" '> '
 end
