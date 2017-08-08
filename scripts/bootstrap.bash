b37c76e5804225a15ca0f2c2b13b34fd4e4b7e6f
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2017-08-08 17:05:19 -0600

    Clean up scripts, remove brew management experiments

--- scripts/bootstrap.bash
+++ scripts/bootstrap.bash
@@ -7,11 +7,6 @@ if ! command -v git > /dev/null ; then
 fi
 
 git clone --depth 1 git@github.com:nthapaliya/dotfiles.git "$HOME/.dotfiles"
-git clone --depth 1 \
-  -b niraj_custom \
-  --single-branch \
-  --recursive \
-  git@github.com:nthapaliya/prezto.git "$HOME/.zprezto"
 
 if [[ $(uname) == 'Darwin' ]]; then
   # clone mac os bootstrap
--- scripts/macOS-bootstrap.bash
+++ scripts/macOS-bootstrap.bash
@@ -5,7 +5,7 @@
 # read this before running it.
 # The script is not tested: In fact its best to run these line by line, manually
 
-: ${DOTFILES_ROOT:="$HOME/.dotfiles"}
+: "${DOTFILES_ROOT:="$HOME/.dotfiles"}"
 
 # Download and and install homebrew
 if ! command -v brew > /dev/null; then
@@ -18,39 +18,46 @@ if ! brew doctor > /dev/null; then
   exit 1
 fi
 
-# Read brewfile and install everything (reconsider this)
-# --global expects ~/.Brewfile
-# if it is not there you can specify a file with --file <filename>
-if ! brew bundle check --global &> /dev/null ; then
-  brew bundle --global
-fi
+brew install \
+  coreutils \
+  curl \
+  fish \
+  fzf \
+  geos \
+  git \
+  htop \
+  imagemagick \
+  jq \
+  mysql \
+  neovim \
+  rbenv \
+  reattach-to-user-namespace \
+  redis \
+  shellcheck \
+  stow \
+  the_silver_searcher \
+  tmate \
+  tmux \
+  tree \
+  wget \
+  yarn \
+;
 
-# just in case stow is not in the brewfile
-if ! command -v stow > /dev/null; then
-  brew install stow
-fi
+brew cask install \
+  1password \
+  cakebrew \
+  daisydisk \
+  dash \
+  emacs \
+  hipchat \
+  imageoptim \
+  iterm2 \
+  selfcontrol \
+  the-unarchiver \
+  transmission \
+  vlc \
+;
+# TODO: list other must-have packages here
 
 # will probably fail for the first time because of default bashrc
 stow -R --dir="$DOTFILES_ROOT" --target="$HOME" home
-
-# Download, rbenv and some useful plugins
-# probably want to change the default, which is ~/.rbenv
-if ! command -v rbenv > /dev/null ; then
-  rbenv_root="${RBENV_ROOT:-$HOME/.rbenv}"
-  plugins_dir="$rbenv_root/plugins"
-
-  # install rbenv
-  git clone --depth 1 https://github.com/rbenv/rbenv.git "$rbenv_root"
-
-  # Optionally, try to compile dynamic bash extension to speed up rbenv.
-  cd "$rbenv_root" && src/configure && make -C src
-
-  mkdir -p "$plugins_dir"
-
-  # install plugins
-  git clone --depth 1 https://github.com/rbenv/ruby-build.git "$plugins_dir/ruby-build"
-  git clone --depth 1 https://github.com/tpope/rbenv-ctags.git "$plugins_dir/rbenv-ctags"
-  git clone --depth 1 https://github.com/rkh/rbenv-update.git "$plugins_dir/rbenv-update"
-fi
-
-# TODO: zsh helpers/link-prezto
