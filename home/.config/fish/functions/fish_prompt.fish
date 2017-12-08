52388709183fb9f35f878fbd196a1a0ecc93ef47
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2017-12-08 15:07:30 -0600

    fish_prompt: show dirty state, take 2

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,19 +1,33 @@
 function fish_prompt --description 'Write out the prompt'
-    set __fish_git_prompt_char_dirtystate '*'
-    set __fish_git_prompt_showdirtystate 'yes'
-    set __fish_git_prompt_showcolorhints 'yes'
-
     set -l last_status $status
-    if test $last_status -ne 0
-      set prompt_last_status " [$last_status]"
-    else
-      set prompt_last_status
+
+    if not set -q __fish_git_prompt_char_dirtystate
+        set -g __fish_git_prompt_char_dirtystate '*'
     end
 
-    set -l cyan   (set_color cyan)
-    set -l green  (set_color green)
-    set -l red    (set_color red)
-    set -l normal (set_color normal)
+    if not set -q __fish_git_prompt_showdirtystate
+        set -g __fish_git_prompt_showdirtystate 'yes'
+    end
 
-    echo -n -s "$cyan" (prompt_pwd) "$green" (__fish_vcs_prompt) "$red" "$prompt_last_status" "$normal" '> '
+    if not set -q __fish_git_prompt_showcolorhints
+        set -g __fish_git_prompt_showcolorhints 'yes'
+    end
+
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch green --bold
+    end
+
+    set_color cyan
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    printf '%s' (__fish_vcs_prompt)
+
+    if test $last_status -ne 0
+        set_color red
+        echo -n "[$last_status]"
+    end
+
+    set_color normal
+    echo -n '> '
 end
