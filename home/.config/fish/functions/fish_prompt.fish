30fb03de4593cf7518d3db6ae85880623dada383
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2018-01-03 14:27:10 -0600

    fish_prompt: move global variable check outside function

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,21 +1,21 @@
+if not set -q __fish_git_prompt_char_dirtystate
+    set -g __fish_git_prompt_char_dirtystate '*'
+end
+
+if not set -q __fish_git_prompt_showdirtystate
+    set -g __fish_git_prompt_showdirtystate 'yes'
+end
+
+if not set -q __fish_git_prompt_showcolorhints
+    set -g __fish_git_prompt_showcolorhints 'no'
+end
+
+if not set -q __fish_git_prompt_color_branch
+    set -g __fish_git_prompt_color_branch green --bold
+end
+
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
-
-    if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate '*'
-    end
-
-    if not set -q __fish_git_prompt_showdirtystate
-        set -g __fish_git_prompt_showdirtystate 'yes'
-    end
-
-    if not set -q __fish_git_prompt_showcolorhints
-        set -g __fish_git_prompt_showcolorhints 'yes'
-    end
-
-    if not set -q __fish_git_prompt_color_branch
-        set -g __fish_git_prompt_color_branch green --bold
-    end
 
     if set -q SSH_CLIENT
         or set -q SSH_TTY
@@ -25,8 +25,8 @@ function fish_prompt --description 'Write out the prompt'
     printf '%s%s%s%s' (set_color cyan) (prompt_pwd) (set_color normal) (__fish_vcs_prompt)
 
     if test $last_status -ne 0
-        printf '%s%s' (set_color red) "[$last_status]"
+        printf '%s[%s]' (set_color red) $last_status
     end
 
-    printf '%s%s' (set_color normal) '> '
+    printf '%s> ' (set_color normal)
 end
