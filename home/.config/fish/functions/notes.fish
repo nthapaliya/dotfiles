be2e3e61506e9c358e5c883dcaf2d5805d5fe9ff
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2018-08-27 16:48:03 -0600

    notes.fish: set tw=72 for notes files

--- home/.config/fish/functions/notes.fish
+++ home/.config/fish/functions/notes.fish
@@ -1,13 +1,13 @@
 function notes
     set -l DAILY_DIR "$HOME/OSS/notes/daily2"
     if not count $argv >/dev/null
-        eval $EDITOR $DAILY_DIR/( gdate +%F ).md
+        nvim -c 'set tw=72' $DAILY_DIR/( gdate +%F ).md
     else
         if test $argv[1] = '-m'
             echo - ( gdate +%H:%M ) - $argv[2..-1] >>$DAILY_DIR/( gdate +%F ).md
             return
         end
 
-        eval $EDITOR $DAILY_DIR/( gdate +%F -d "$argv" ).md
+        nvim -c 'set tw=72' $DAILY_DIR/( gdate +%F -d "$argv" ).md
     end
 end
