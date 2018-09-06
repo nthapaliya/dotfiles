d3da79691d3e9dc0e13eb4c55c6aea77cea11476
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2018-09-06 10:21:25 -0600

    Make "al" interactive

--- home/.config/fish/functions/al.fish
+++ home/.config/fish/functions/al.fish
@@ -1,3 +1,18 @@
 function al
-    rg --files-with-matches --smart-case $argv
+    # rg --files-with-matches --smart-case $argv
+    rg \
+        --color=always \
+        --no-heading \
+        --smart-case \
+        $argv |
+    __fzf_down \
+        --bind="ctrl-e:execute( nvim {+1} -c'/$argv' )" \
+        --delimiter=: \
+        --preview '' |
+    cut -d: -f1 | sort | uniq
 end
+
+# TODO:
+# Add description
+# Add caption/hint/header showing ctrl-e is an option
+# Play with this for a while, see how it feels. Maybe rename function
