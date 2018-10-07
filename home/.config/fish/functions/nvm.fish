4e59c704297844e7af63d3a2c6932d8f8ee0f0a7
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2018-10-07 15:50:11 -0600

    Rewrite nvm.fish a bit: make it alter the universal function instead

--- home/.config/fish/functions/nvm.fish
+++ home/.config/fish/functions/nvm.fish
@@ -1,46 +1,51 @@
-function nvm-fast
+function nvm-use
+    set node_versions_path $NVM_DIR/versions/node
+    set target_version $argv[1]
+
+    set clean_path (string match -ev "$node_versions_path" $PATH)
+    set clean_fish_user_paths (string match -ev "$node_versions_path" $fish_user_paths)
+
+    if test "$target_version" = 'system'
+        # set -gx PATH $clean_path
+        set -U fish_user_paths $clean_fish_user_paths
+        return 0
+    end
+
+    if test -z "$target_version"
+        if test -f .nvmrc
+            set target_version (cat .nvmrc)
+        else
+            echo "Error: No version provided and no .nvmrc found"
+            return 1
+        end
+    end
+
+    set matched_version (bash -c "source $NVM_DIR/nvm.sh --no-use; nvm_version $target_version")
+
+    if test -z "$matched_version" -o "$matched_version" = 'N/A'
+        echo "No version installed for $target_version, run nvm install $target_version"
+        echo "Installed versions: "
+        for file in $node_versions_path/v*
+            echo ' -' $file
+        end
+        return 1
+    end
+
+    # set -gx PATH $node_versions_path/$matched_version/bin $clean_path
+    set -U fish_user_paths $node_versions_path/$matched_version/bin $clean_fish_user_paths
+end
+
+function nvm
     set -q NVM_DIR
-    or set -l NVM_DIR ~/.nvm
-    set -l brigand_nvm_fish_path $NVM_DIR/versions/node
+    or set -g NVM_DIR ~/.nvm
+
     if test (count $argv[1]) -lt 1
         echo 'nvm-fast: at least one argument is required'
     end
 
-    set -l command $argv[1]
-    if test $command = 'use'
-        set target_version "unknown"
-        if test (count $argv) -eq 1
-            if test -f .nvmrc
-                set target_version (cat .nvmrc)
-            else
-                echo "Error: No version provided and no .nvmrc found"
-                return
-            end
-        else
-            set target_version $argv[2]
-        end
-        set -l target_version $target_version
-
-        set -l matched_version (bash -c "source $NVM_DIR/nvm.sh --no-use; nvm_version $target_version")
-
-        if test -z $matched_version -o $matched_version = 'N/A'
-            echo "No version installed for $target_version, run nvm install $target_version"
-            echo "Installed versions: "
-            for file in $brigand_nvm_fish_path/v*
-                echo ' -' $file
-            end
-        else
-            set -l new_path (string match -ev "$brigand_nvm_fish_path" $PATH)
-            if test $matched_version != 'system'
-                set new_path $brigand_nvm_fish_path/$matched_version/bin $new_path
-            end
-            set -gx PATH $new_path
-        end
+    if test $argv[1] = 'use'
+        nvm-use $argv[2..-1]
     else
         bash -c "source $NVM_DIR/nvm.sh --no-use; nvm $argv"
     end
-end
-
-function nvm
-    nvm-fast $argv
 end
