938fa284bfad5b58bb851d9004eff5d3c5833410
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2019-09-29 09:29:00 -0600

    Some tweaks to `notes`

--- home/.config/fish/functions/notes.fish
+++ home/.config/fish/functions/notes.fish
@@ -1,10 +1,17 @@
 function notes
     set DAILY_DIR "$HOME/OSS/notes/daily2"
+    set todays_notes_file $DAILY_DIR/( date +%F ).md
+
     if not count $argv >/dev/null
-        nvim -c 'set tw=72' $DAILY_DIR/( date +%F ).md
+        nvim -c 'set tw=72' $todays_notes_file
     else
         if test $argv[1] = '-m'
-            echo - ( date +%H:%M ) - $argv[2..-1] >>$DAILY_DIR/( date +%F ).md
+            echo - ( date +%H:%M ) - $argv[2..-1] >>$todays_notes_file
+            return
+        end
+
+        if test $argv[1] = 'standup'
+            nvim -c 'set tw=72' -O (echo $DAILY_DIR/*.md | awk '{ print $NF }') $todays_notes_file
             return
         end
 
