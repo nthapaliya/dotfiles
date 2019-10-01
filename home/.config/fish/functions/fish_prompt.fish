f3779a48a1b66f804bbe1fd67461f25d77aa5609
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2019-10-01 13:53:41 -0600

    prompt: Clean up setting fish_git_prompt values

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,22 +1,8 @@
-if not set -q __fish_git_prompt_char_dirtystate
-    set -g __fish_git_prompt_char_dirtystate '*'
-end
-
-if not set -q __fish_git_prompt_showdirtystate
-    set -g __fish_git_prompt_showdirtystate 'yes'
-end
-
-if not set -q __fish_git_prompt_showcolorhints
-    set -g __fish_git_prompt_showcolorhints 'no'
-end
-
-if not set -q __fish_git_prompt_color_branch
-    set -g __fish_git_prompt_color_branch green --bold
-end
-
-if not set -q __fish_git_prompt_shorten_branch_len
-    set -g __fish_git_prompt_shorten_branch_len 15
-end
+set -g __fish_git_prompt_char_dirtystate '*'
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showcolorhints 'no'
+set -g __fish_git_prompt_color_branch green --bold
+set -g __fish_git_prompt_shorten_branch_len 15
 
 function fish_prompt --description 'Write out the prompt'
     set last_status $status
