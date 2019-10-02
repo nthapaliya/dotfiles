c344cc0b0104b4a866f4f41b1f503eb4534612f8
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2019-10-02 13:14:53 -0600

    peek: make `edit` the default action

--- home/.config/fish/functions/peek.fish
+++ home/.config/fish/functions/peek.fish
@@ -1,7 +1,4 @@
 function peek
-    argparse 'e/edit' -- $argv
-    or return
-
     set name $argv[1]
 
     set TYPE ( type -t $name )
@@ -10,24 +7,10 @@ function peek
     and return
 
     if test "$TYPE" = "file"
-        if test -n "$_flag_e"
-            e ( type --force-path $name )
-            return
-        end
-
-        type $name
-        echo -----------------------------------------------------------------
-        less -FX ( type --force-path $name )
-
+        e ( type --force-path $name )
         return
     end
 
-    if test -n "$_flag_edit"
-        # e ( functions $name | head -1 | cut -c14- | cut -d ' ' -f1 )
-        eval ( functions $name | head -1 | cut -c14- | awk '{ print "nvim", $1, "+" $NF }' )
-
-        return
-    end
-
-    type $name
+    # e ( functions $name | head -1 | cut -c14- | cut -d ' ' -f1 )
+    eval ( functions $name | head -1 | cut -c14- | awk '{ print "nvim", $1, "+" $NF }' )
 end
