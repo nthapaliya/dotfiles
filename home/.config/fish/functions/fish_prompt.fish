fe1ba9e0fe0ddfe8c6c927219720258392caa2d2
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2019-10-02 18:41:26 -0600

    config.fish: add a few lines to configure __fish_git_prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,12 @@
-set -g __fish_git_prompt_char_dirtystate '*'
-set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_showcolorhints 'no'
-set -g __fish_git_prompt_color_branch green --bold
+set -g __fish_git_prompt_color_branch 'green'
+set -g __fish_git_prompt_describe_style 'branch'
 set -g __fish_git_prompt_shorten_branch_len 15
+set -g __fish_git_prompt_showcolorhints 'no'
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showuntrackedfiles 'yes'
+
+set -g __fish_git_prompt_char_dirtystate '*'
+set -g __fish_git_prompt_char_untrackedfiles '*'
 
 function fish_prompt --description 'Write out the prompt'
     set last_status $status
@@ -13,7 +17,7 @@ function fish_prompt --description 'Write out the prompt'
         printf '%s%s@%s ' (set_color yellow) (whoami) (prompt_hostname)
     end
 
-    printf '%s%s%s%s' (set_color cyan) (prompt_pwd) (set_color normal) (__fish_vcs_prompt)
+    printf '%s%s%s%s' (set_color cyan) (prompt_pwd) (set_color normal) (__fish_git_prompt)
 
     if test $last_status -ne 0
         printf '%s[%s]' (set_color red) $last_status
