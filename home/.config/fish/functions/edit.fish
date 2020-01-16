754e9fd7f0f8dd7ec10b1202ce63ab208850d793
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2020-01-16 11:28:46 -0600

    Distill edit.fish to a one-liner
    Maybe future-me will be looking at this commit trying
    to understand what the hell is going on, but current-me
    is quite satisfied with this type of line-golfing

--- home/.config/fish/functions/edit.fish
+++ home/.config/fish/functions/edit.fish
@@ -1,17 +1,3 @@
-function edit --description 'alias edit peek'
-
-    set name $argv[1]
-
-    set TYPE ( type -t $name )
-
-    test -z "$TYPE"
-    and return
-
-    if test "$TYPE" = "file"
-        e ( type --force-path $name )
-        return
-    end
-
-    # e ( functions $name | head -1 | cut -c14- | cut -d ' ' -f1 )
-    eval ( functions $name | head -1 | cut -c14- | awk '{ print "vi", $1, "+" $NF }' )
+function edit --description 'edit fish functions and bash scripts in $PATH'
+    e ( type --force-path $argv[1] 2>/dev/null || functions --details $argv[1] | grep -v 'n/a' )
 end
