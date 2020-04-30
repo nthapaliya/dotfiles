ab07d7114fb36cb5a0fdda25c53e942406510389
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2020-04-30 15:53:37 -0600

    Remove fish_variables from repo

---
+++ home/.config/fish/functions/set_colors.fish
@@ -0,0 +1,15 @@
+function set_colors
+    set fish_color_autosuggestion 555
+    set fish_color_cancel normal
+    set fish_color_command eeeeee
+    set fish_color_cwd 008000
+    set fish_color_cwd_root 800000
+    set fish_color_error ff5f5f
+    set fish_color_history_current normal
+    set fish_color_match normal
+    set fish_color_search_match ffff00
+    set fish_color_selection c0c0c0
+    set fish_color_user 00ff00
+    set fish_color_valid_path normal
+    set fish_pager_color_completion normal
+end
