86894b930cddcc2b63b0f4e78cad9c1d87aedee5
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2020-05-04 11:49:01 -0600

    Remove interactive bits from vcommit and vdirty
    I almost never use them

--- home/.config/fish/functions/vdirty.fish
+++ home/.config/fish/functions/vdirty.fish
@@ -1,26 +1,3 @@
-function __gf
-    set preview_command '
-          if git ls-files --error-unmatch {-1} >/dev/null 2>/dev/null
-            git diff --color {-1} | diff-so-fancy | head -$LINES
-          else
-            set_color green; echo +++ NEW FILE +++ ; set_color normal; echo;
-            bat --color always --paging never {-1} | head -$LINES
-          end;'
-
-    git -c color.status=always status --short -- $argv |
-    fzf --ansi --exit-0 --multi --preview-window=down:80% --reverse \
-        --nth 2..,.. \
-        --preview="$preview_command"
-end
-
 function vdirty
-    argparse 'i/interactive' -- $argv
-
-    set git_args $argv
-
-    if test -n "$_flag_interactive"
-        nvim ( __gf )
-    else
-        nvim ( git status -s -- $git_args | cut -c4- )
-    end
+    nvim ( git status -s -- $argv | cut -c4- )
 end
