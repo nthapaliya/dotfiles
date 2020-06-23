c643ebda77790f89697bd8a1ae38ec2613f7617c
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2020-06-22 22:05:45 -0600

    Raspberrypi + pihole specific updt script

--- home/.config/fish/functions/updt.fish
+++ home/.config/fish/functions/updt.fish
@@ -1,27 +1,12 @@
 function updt
-    if test (uname) != 'Darwin'
-        return 1
-    end
-
-    set brew_cache_dir ~/Library/Caches/Homebrew
-    set hfile $brew_cache_dir/.updated
-
-    test -d $brew_cache_dir; or mkdir -p $brew_cache_dir
+    set hfile ~/.updated
     test -f $hfile; or touch $hfile
 
-    test (math (date +%s) - (date -r $hfile +%s)) -lt (math 8 \* 60 \* 60); and return 1
-
+    test (math (date +%s) - (date -r $hfile +%s)) -lt (math 7 \* 24 \* 60 \* 60); and return 1
     touch $hfile
 
-    echo 'Running curl commands...'
-    # curl --silent -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy >~/.local/bin/diff-so-fancy
-    curl --silent -L https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >~/.config/nvim/autoload/plug.vim
-    # curl --silent -L https://raw.githubusercontent.com/fishpkg/fish-humanize-duration/master/humanize_duration.fish >~/.config/fish/conf.d/humanize_duration.fish
-    # curl --silent -L https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish >~/.config/fish/conf.d/done.fish
-
-    echo 'Running brew commands...'
-    brew bundle dump --force --describe --file=~/.config/brew/Brewfile
-    brew update
-    brew upgrade
-    brew outdated --cask
+    sudo apt update
+    sudo apt upgrade
+    pihole -up
+    pihole restartdns
 end
