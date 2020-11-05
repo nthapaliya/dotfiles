e3070bc08dbd8a0b35d31942bc1299a4ddfa580a
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2020-11-05 12:16:14 -0600

    Update standup zoom link

--- home/.config/fish/functions/standup.fish
+++ home/.config/fish/functions/standup.fish
@@ -2,6 +2,6 @@ function standup
     open "http://bit.ly/2E8BPLa" # PR's
     open "https://www.pivotaltracker.com/n/projects/1421752" # PIV
     open "http://todomvc.com/examples/react/#/"
-    open "http://bit.ly/2WoXFB0" # Zoom link
+    open "https://bit.ly/2Jy6fM1" # Zoom Link
     tmux new-window "notes standup"
 end
