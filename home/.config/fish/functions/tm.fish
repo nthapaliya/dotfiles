cf6576c62a187051641f9821e2ed7930807919cd
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-01-14 21:55:40 -0600

    Remove tm-exec

--- home/.config/fish/functions/tm.fish
+++ home/.config/fish/functions/tm.fish
@@ -1,4 +1,10 @@
+# Set as Terminal.app startup command
+# /usr/local/bin/tmux new-session -A -s 0
+
+# Set as SSH command
+# ssh foo@bar -t '/usr/local/bin/tmux new-session -A -s 0'
+
 function tm
-    tmux attach $argv
-    or tmux new $argv
+    set args $argv 0
+    tmux new-session -A -s $args[1]
 end
