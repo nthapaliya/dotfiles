42e81f17ca66a34d175e7fd34296d16094af1843
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-02-11 21:24:14 -0600

    Remove some curl incantations
    - humanized_duration is now included in done.fish
    - Update done.fish
    - Remove diff-so-fancy direct download

--- home/.config/fish/functions/updt.fish
+++ home/.config/fish/functions/updt.fish
@@ -14,10 +14,8 @@ function updt
     touch $hfile
 
     echo 'Running curl commands...'
-    # curl --silent -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy >~/.local/bin/diff-so-fancy
     curl --silent -L https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >~/.config/nvim/autoload/plug.vim
-    # curl --silent -L https://raw.githubusercontent.com/fishpkg/fish-humanize-duration/master/humanize_duration.fish >~/.config/fish/conf.d/humanize_duration.fish
-    # curl --silent -L https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish >~/.config/fish/conf.d/done.fish
+    curl --silent -L https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish >~/.config/fish/conf.d/done.fish
 
     echo 'Running brew commands...'
     brew bundle dump --force --describe --file=~/.config/brew/Brewfile
