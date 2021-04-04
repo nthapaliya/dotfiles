f53bee5639d7d6bbcbd44dadfaf9aa6580687de0
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-04-04 13:32:23 -0400

    fish_md5 has been renamed to _fish_md5 with latest fish updates

--- home/.config/fish/completions/rake.fish
+++ home/.config/fish/completions/rake.fish
@@ -23,7 +23,7 @@ function __run_rake_completion
 end
 
 function __hashed_rakefiles
-    fish_md5 -s (begin
+    __fish_md5 -s (begin
         echo $PWD
         for file in rakefile Rakefile rakefile.rb Rakefile.rb
             test -e $file
