ef241c7e27eac1f402ab00dbc5d21131ba336a9a
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-07 23:53:34 -0500

    Set opt=true for many plugins
    - Profile slower ones, like cmp, and set them to opt. Configure
      them to load only on certain events.

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -8,7 +8,7 @@ return require("packer").startup({
     use("justinmk/vim-dirvish")
     use({ "rbgrouleff/bclose.vim", disable = true })
     use("rstacruz/vim-closer")
-    use({ "sheerun/vim-polyglot", disable = false })
+    use({ "sheerun/vim-polyglot", disable = true })
     use("tommcdo/vim-lion")
     use({ "tpope/vim-commentary", disable = true })
     use("tpope/vim-fugitive")
@@ -17,11 +17,14 @@ return require("packer").startup({
     use("tpope/vim-surround")
     use("tpope/vim-unimpaired")
     use("unblevable/quick-scope")
-    use("vim-scripts/BufOnly.vim")
+    use({ "vim-scripts/BufOnly.vim", opt = true, cmd = { "BufOnly" } })
     use("wincent/terminus")
+    use("nvim-lua/plenary.nvim")
 
     use({
       "mhartington/formatter.nvim",
+      opt = true,
+      cmd = { "FormatWrite" },
       config = function()
         require("formatter").setup({
           filetype = {
@@ -70,15 +73,10 @@ return require("packer").startup({
     })
 
     use({
-      "andymass/vim-matchup",
-      after = { "nvim-treesitter" },
-      config = function()
-        require("nvim-treesitter.configs").setup({ matchup = { enable = true } })
-      end,
-    })
-
-    use({
       "numToStr/Comment.nvim",
+      opt = true,
+      requires = { "JoosepAlviste/nvim-ts-context-commentstring" },
+      keys = { { "n", "gcc" }, { "n", "gcb" }, { "v", "gc" } },
       config = function()
         require("Comment").setup({
           pre_hook = function(ctx)
@@ -108,66 +106,12 @@ return require("packer").startup({
     })
 
     use({
-      "hrsh7th/nvim-cmp",
-      requires = {
-        "hrsh7th/cmp-nvim-lsp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-      },
-      -- https://github.com/neovim/nvim-lspconfig/wiki/Snippets
-      config = function()
-        local luasnip = require("luasnip")
-        local cmp = require("cmp")
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require("luasnip").lsp_expand(args.body)
-            end,
-          },
-          mapping = {
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            }),
-            ["<Tab>"] = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-              else
-                fallback()
-              end
-            end,
-            ["<S-Tab>"] = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-              else
-                fallback()
-              end
-            end,
-          },
-          sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
-        })
-      end,
-    })
-
-    use({
       "williamboman/nvim-lsp-installer",
-      requires = { "neovim/nvim-lspconfig" },
-      after = {
-        "nvim-lspconfig",
-        "cmp-nvim-lsp",
+      requires = {
+        { "neovim/nvim-lspconfig", before = "nvim-lsp-installer" },
+        { "hrsh7th/cmp-nvim-lsp", before = "nvim-lsp-installer" },
       },
       config = function()
-        -- local nvim_lsp = require('lspconfig')
         -- https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
         local on_attach = function(_, bufnr)
           local function buf_set_keymap(...)
@@ -207,9 +151,8 @@ return require("packer").startup({
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-        -- copied from: https://github.com/williamboman/nvim-lsp-installer#setup
-        local lsp_installer = require("nvim-lsp-installer")
 
+        local lsp_installer = require("nvim-lsp-installer")
         lsp_installer.on_server_ready(function(server)
           local opts = {
             on_attach = on_attach,
@@ -225,9 +168,8 @@ return require("packer").startup({
             },
           }
 
-          local server_specific_settings = {}
           if server.name == "sumneko_lua" then
-            server_specific_settings = {
+            opts.settings = {
               Lua = {
                 runtime = {
                   version = "LuaJIT",
@@ -248,8 +190,6 @@ return require("packer").startup({
                 },
               },
             }
-
-            opts.settings = server_specific_settings
           end
 
           server:setup(opts)
@@ -259,7 +199,9 @@ return require("packer").startup({
 
     use({
       "lewis6991/gitsigns.nvim",
-      requires = { "nvim-lua/plenary.nvim" },
+      opt = true,
+      before = { "nvim-lua/plenary.nvim" },
+      event = "VimEnter",
       config = function()
         require("gitsigns").setup({
           signs = {
@@ -278,8 +220,8 @@ return require("packer").startup({
             ["n <leader>hr"] = "<cmd>Gitsigns undo_stage_hunk<CR>",
             ["n <leader>hs"] = "<cmd>Gitsigns stage_hunk<CR>",
             ["n <leader>hu"] = "<cmd>Gitsigns reset_hunk<CR>",
-            ["v <leader>hs"] = ":Gitsigns stage_hunk<CR>",
-            ["v <leader>hu"] = ":Gitsigns reset_hunk<CR>",
+            ["v <leader>hs"] = "<cmd>Gitsigns stage_hunk<CR>",
+            ["v <leader>hu"] = "<cmd>Gitsigns reset_hunk<CR>",
           },
         })
       end,
@@ -287,6 +229,8 @@ return require("packer").startup({
 
     use({
       "nvim-lualine/lualine.nvim",
+      opt = true,
+      event = "VimEnter",
       config = function()
         require("lualine").setup({
           options = {
@@ -306,7 +250,9 @@ return require("packer").startup({
 
     use({
       "nvim-telescope/telescope.nvim",
-      requires = { { "nvim-lua/plenary.nvim" } },
+      opt = true,
+      before = { "nvim-lua/plenary.nvim" },
+      cmd = { "Telescope" },
       config = function()
         require("telescope").setup({
           defaults = {
@@ -328,6 +274,8 @@ return require("packer").startup({
 
     use({
       "folke/tokyonight.nvim",
+      opt = true,
+      event = "VimEnter",
       branch = "main",
       config = function()
         vim.g.tokyonight_colors = {
@@ -341,11 +289,29 @@ return require("packer").startup({
       end,
     })
 
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use({ "nvim-treesitter/nvim-treesitter-textobjects", run = ":TSUpdate" })
+    use({
+      "nvim-treesitter/nvim-treesitter",
+      requires = {
+        "nvim-treesitter/nvim-treesitter-refactor",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+      },
+      run = ":TSUpdate",
+    })
+
+    use({
+      "andymass/vim-matchup",
+      after = "nvim-treesitter",
+      config = function()
+        require("nvim-treesitter.configs").setup({ matchup = { enable = true } })
+      end,
+    })
+
     use({ "tpope/vim-dispatch", opt = true, cmd = { "Dispatch", "Make", "Focus", "Start" } })
+
     use({
       "folke/trouble.nvim",
+      opt = true,
+      cmd = { "Trouble", "TroubleToggle" },
       config = function()
         require("trouble").setup({
           icons = false,
@@ -362,8 +328,77 @@ return require("packer").startup({
         })
       end,
     })
+
+    use({
+      { "L3MON4D3/LuaSnip", before = "nvim-cmp" },
+      { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" },
+      { "hrsh7th/cmp-path", after = "nvim-cmp" },
+      { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
+    })
+
+    use({
+      "hrsh7th/nvim-cmp",
+      opt = true,
+      event = "InsertEnter *",
+      config = function()
+        -- Set completeopt to have a better completion experience
+        vim.o.completeopt = "menuone,noselect"
+
+        -- luasnip setup
+        local luasnip = require("luasnip")
+
+        -- nvim-cmp setup
+        local cmp = require("cmp")
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+          mapping = {
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.close(),
+            ["<CR>"] = cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            }),
+            ["<Tab>"] = function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end,
+            ["<S-Tab>"] = function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end,
+          },
+          sources = {
+            { name = "buffer" },
+            { name = "luasnip" },
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = "path" },
+          },
+        })
+      end,
+    })
   end,
   config = {
     display = { open_fn = require("packer.util").float },
+    profile = { enable = true },
   },
 })
