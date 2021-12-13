fea21aa63172c87ceb9999deb1cf0a463aa0e96e
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-12 20:57:24 -0500

    fish_indent now defaults to removing doublequotes in certain situations

--- home/.config/fish/functions/edit.fish
+++ home/.config/fish/functions/edit.fish
@@ -1,7 +1,7 @@
 function edit --description 'edit fish functions and bash scripts in $PATH'
     set name $argv[1]
 
-    if test "$name" = "config"
+    if test "$name" = config
         set config ~/.config/fish/config.fish
         nvim $config
         and source $config
@@ -13,7 +13,7 @@ function edit --description 'edit fish functions and bash scripts in $PATH'
     test -z "$TYPE"
     and return
 
-    if test "$TYPE" = "file"
+    if test "$TYPE" = file
         nvim ( type --force-path $name )
         return
     end
