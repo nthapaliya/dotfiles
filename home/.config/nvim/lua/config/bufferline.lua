06d9b33cc2bca83b59e82949d53bd7780341eb07
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-18 01:28:53 -0500

    While I use nerd-fonts, allow default eye-candy for some plugins

--- home/.config/nvim/lua/config/bufferline.lua
+++ home/.config/nvim/lua/config/bufferline.lua
@@ -1,13 +1,14 @@
 return function()
-  require("bufferline").setup({
-    options = {
-      -- diagnostics = "nvim_lsp", -- This might be too noisy
-      indicator_icon = "▎",
-      buffer_close_icon = "✖",
-      modified_icon = "●",
-      close_icon = "✖",
-      left_trunc_marker = "«",
-      right_trunc_marker = "»",
-    },
-  })
+  require("bufferline").setup({})
+  -- require("bufferline").setup({
+  --   options = {
+  --     -- diagnostics = "nvim_lsp", -- This might be too noisy
+  --     indicator_icon = "▎",
+  --     buffer_close_icon = "✖",
+  --     modified_icon = "●",
+  --     close_icon = "✖",
+  --     left_trunc_marker = "«",
+  --     right_trunc_marker = "»",
+  --   },
+  -- })
 end
--- home/.config/nvim/lua/config/trouble.lua
+++ home/.config/nvim/lua/config/trouble.lua
@@ -1,16 +1,17 @@
 return function()
-  require("trouble").setup({
-    icons = false,
-    fold_open = "▼",
-    fold_closed = "▶",
-    indent_lines = false,
-    -- TODO: find some nice unicode icons for this
-    signs = {
-      error = "error",
-      warning = "warn",
-      hint = "hint",
-      information = "info",
-    },
-    use_lsp_diagnostic_signs = false,
-  })
+  require("trouble").setup({})
+  -- require("trouble").setup({
+  --   icons = false,
+  --   fold_open = "▼",
+  --   fold_closed = "▶",
+  --   indent_lines = false,
+  --   -- TODO: find some nice unicode icons for this
+  --   signs = {
+  --     error = "error",
+  --     warning = "warn",
+  --     hint = "hint",
+  --     information = "info",
+  --   },
+  --   use_lsp_diagnostic_signs = false,
+  -- })
 end
