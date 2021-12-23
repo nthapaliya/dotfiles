354236370ce3ab71ac0947e81dd0fb8c88fba0d7
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-22 23:30:49 -0500

    Cleanup in tmux.conf and others

--- home/.config/nvim/lua/config/lsp_on_attach.lua
+++ home/.config/nvim/lua/config/lsp_on_attach.lua
@@ -25,20 +25,6 @@ M.on_attach = function(client, bufnr)
   buf_set_keymap("n", "<leader>j", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
   -- buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   buf_set_keymap("n", "<leader>ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-          augroup lsp_document_highlight
-            autocmd! * <buffer>
-            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-          augroup END
-        ]],
-      false
-    )
-  end
 end
 
 return M
