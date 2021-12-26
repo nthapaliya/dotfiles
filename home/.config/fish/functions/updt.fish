67d79b8e29fd472be7008a4415cec7bb5c7f5002
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-26 02:55:10 -0500

    Rewrite `updt`, separate Darwin vs OS agnostic commands

--- home/.config/fish/functions/updt.fish
+++ home/.config/fish/functions/updt.fish
@@ -1,40 +1,47 @@
 function updt
-    if test (uname) != Darwin
-        return 1
+    __checktime; or return 1
+
+    __updt_os_agnostic
+
+    if test (uname) = Darwin
+        echo 'Running brew commands...'
+        brew bundle dump --force --describe --file=~/.config/brew/Brewfile
+        brew update
+        brew upgrade
     end
+end
 
-    set brew_cache_dir ~/Library/Caches/Homebrew
-    set hfile $brew_cache_dir/.updated
+function __checktime
+    set hfile ~/.local/share/updt/last_updated
+    mkdir -p (dirname $hfile)
 
-    test -d $brew_cache_dir; or mkdir -p $brew_cache_dir
-    test -f $hfile; or touch $hfile
+    if not test -f $hfile
+        touch $hfile
+        return 0
+    end
 
     test (math (date +%s) - (date -r $hfile +%s)) -lt (math 8 \* 60 \* 60); and return 1
 
     touch $hfile
-
-
-    echo
-
-    # Run this manually
-    # __update_others
-
-    echo 'Running brew commands...'
-    brew bundle dump --force --describe --file=~/.config/brew/Brewfile
-    brew update
-    brew upgrade
+    return 0
 end
 
-function __update_others
-    echo Updating `done.fish`
-    set -l dotfiles ~/Projects/dotfiles/home
-    set -l done_stuff \
-        https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish \
-        $dotfiles/.config/fish/conf.d/done.fish
-    curl --silent -L $done_stuff[1] >$done_stuff[2]
+function __updt_os_agnostic
+    echo 'Updating `done.fish`'
+    curl --silent -L --remote-name \
+        --output-dir ~/Projects/dotfiles/home/.config/fish/conf.d/ \
+        https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish
+    echo 'Updating `plug.vim`'
+    curl --silent -L --remote-name \
+        --output-dir ~/Projects/dotfiles/home/.vim/autoload/ \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-    echo 'Updating neovim plugins'
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c PackerSync
-    nvim --headless -c "LspInstall --sync solargraph bashls eslint rust_analyzer tailwindcss vimls tsserver sumneko_lua" -c q
+    if command -sq nvim
+        echo 'Updating neovim plugins'
+        nvim -c 'autocmd User PackerComplete quitall' -c PackerSync
+    end
+    if command -sq vim
+        echo 'Updating vim plugins'
+        vim +PlugUpdate +PlugUpgrade +qall
+    end
 end
