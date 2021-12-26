c1e89c7fb6080db510db1417f853c4de28343560
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2021-12-25 21:56:26 -0500

    Remove neorg, for now.

--- home/.config/nvim/lua/config/nvim-treesitter.lua
+++ home/.config/nvim/lua/config/nvim-treesitter.lua
@@ -1,22 +1,8 @@
 return function()
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.org = {
-    install_info = {
-      url = "https://github.com/milisims/tree-sitter-org",
-      revision = "f110024d539e676f25b72b7c80b0fd43c34264ef",
-      files = { "src/parser.c", "src/scanner.cc" },
-    },
-    filetype = "org",
-  }
-
   require("nvim-treesitter.configs").setup({
     ensure_installed = "maintained",
     indent = { enable = true },
-    highlight = {
-      enable = true,
-      disable = { "org" },
-      additional_vim_regex_highlighting = { "org" },
-    },
+    highlight = { enable = true, },
   })
 
   vim.wo.foldlevel = 20
