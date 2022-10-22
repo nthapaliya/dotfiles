39ff5191797f133fc13c7ef479b462f6599a81e8
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2022-10-22 10:54:24 -0400

    docs: Emit a message when fish needs to be setup for the first time

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,7 @@
-set -q fish_vars_set; or set_up_fish
+if not set -q fish_vars_set
+    echo It looks like fish has not been set up yet, run:
+    echo \$ set_up_fish
+    echo to finish up initializing abbrs, colors and universal vars.
+end
 
 # set -gx NODE_OPTIONS --openssl-legacy-provider
