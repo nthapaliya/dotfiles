b62c5f9831455dafa59ec6ebd4f63e361e063cb2
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2022-10-22 06:22:28 -0400

    refactor: Offload most of 'updt.fish' to topgrade

--- home/.config/fish/functions/updt.fish
+++ home/.config/fish/functions/updt.fish
@@ -1,18 +1,14 @@
 function updt
     __checktime; or return 1
 
-    __updt_os_agnostic
-
     if test (uname) = Darwin
         echo 'Running brew commands...'
         brew bundle dump --force --describe --file=~/.config/brew/Brewfile
-        brew update
-        brew upgrade
-    else if test (uname) = Linux
-        echo "Updating and upgrading apt"
-        echo "sudo apt update && sudo apt upgrade"
-        sudo apt update && sudo apt upgrade
     end
+
+    topgrade-rs
+    # On Mac, use this
+    # topgrade
 end
 
 function __checktime
@@ -28,27 +24,4 @@ function __checktime
 
     touch $hfile
     return 0
-end
-
-function __updt_os_agnostic
-    if not set -q SSH_CLIENT
-        echo 'Updating `done.fish`'
-        cd ~/Projects/dotfiles/home/.config/fish/conf.d/
-        curl --silent -L --remote-name \
-            https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish
-        cd ~/Projects/dotfiles/home/.vim/autoload/
-        echo 'Updating `plug.vim`'
-        curl --silent -L --remote-name \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        cd -
-    end
-
-    if command -sq nvim
-        echo 'Updating neovim plugins'
-        nvim --headless -c 'autocmd User PackerComplete quitall' -c PackerSync
-    end
-    if command -sq vim
-        echo 'Updating vim plugins'
-        vim +PlugUpdate +qall
-    end
 end
