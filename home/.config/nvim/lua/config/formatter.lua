7291426cc74b155d4e3cea478646233b3f7e947c
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2022-10-23 02:03:17 -0400

    refactor: simplify formatter.nvim configuration

--- home/.config/nvim/lua/config/formatter.lua
+++ home/.config/nvim/lua/config/formatter.lua
@@ -1,35 +1,10 @@
 return function()
   require("formatter").setup({
     filetype = {
-      json = {
-        function()
-          return {
-            exe = "jq",
-            args = { "." },
-            stdin = true,
-          }
-        end,
-      },
-      fish = {
-        function()
-          return {
-            exe = "fish_indent",
-            stdin = true,
-          }
-        end,
-      },
-      lua = {
-        function()
-          return {
-            exe = "stylua",
-            args = { "--indent-type", "Spaces", "--indent-width", 2, "-" },
-            stdin = true,
-          }
-        end,
-      },
-      ["*"] = {
-        require("formatter.filetypes.any").remove_trailing_whitespace,
-      },
+      fish = { require("formatter.filetypes.fish").fishindent },
+      json = { require("formatter.filetypes.json").jq },
+      lua = { require("formatter.filetypes.lua").stylua },
+      ["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
     },
   })
 end
