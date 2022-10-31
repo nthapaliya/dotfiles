3f8355c13b249f3b83c572af395aa44cc01a773d
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2022-10-31 00:35:12 -0400

    fix: config for lua language server

--- home/.config/nvim/lua/config/lsp-zero.lua
+++ home/.config/nvim/lua/config/lsp-zero.lua
@@ -1,6 +1,15 @@
 return function()
   local lsp = require("lsp-zero")
   lsp.preset("recommended")
+  lsp.ensure_installed({
+    "tsserver",
+    "eslint",
+    "sumneko_lua",
+  })
+  lsp.configure("sumneko_lua", {
+    -- Get the language server to recognize the `vim` global
+    settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+  })
 
   lsp.setup()
 end
