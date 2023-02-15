af0e576a69070b254b37c32497eb0df6232b56be
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2023-02-15 10:56:21 -0500

    Lualine

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -51,6 +51,8 @@ return {
         integrations = {
           gitsigns = true,
           treesitter = true,
+          navic = { enabled = true },
+          native_lsp = { enabled = true },
           -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       })
@@ -60,71 +62,6 @@ return {
   },
 
   -- UI/cosmetic
-
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = {
-      options = {
-        -- icons_enabled = false,
-        section_separators = " ",
-        component_separators = " ",
-      },
-      sections = {
-        lualine_a = {
-          {
-            "mode",
-            fmt = function(str)
-              return str:sub(1, 1)
-            end,
-          },
-        },
-        lualine_b = {
-          "branch",
-          "diff",
-          {
-            "diagnostics",
-            on_click = function()
-              vim.diagnostic.setqflist()
-            end,
-          },
-        },
-        lualine_c = { { "filename", path = 1 } },
-        -- lualine_x = { "filetype", "encoding", "fileformat" },
-        -- lualine_y = { "progress" },
-        -- lualine_z = { "location" },
-      },
-      tabline = {
-        lualine_a = { "buffers" },
-        -- lualine_b = {},
-        -- lualine_c = {},
-        -- lualine_x = {},
-        -- lualine_y = {},
-        -- lualine_z = { { "filename", path = 1 } },
-      },
-      -- winbar = {
-      --   lualine_a = {
-      --     {
-      --       function()
-      --         return " "
-      --       end,
-      --       color = { bg = "NONE" },
-      --     },
-      --     {
-      --       function()
-      --         return require("nvim-navic").get_location()
-      --       end,
-      --       cond = function()
-      --         return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-      --       end,
-      --       color = { bg = "NONE" },
-      --     },
-      --   },
-      -- },
-    },
-  },
-
   {
     "SmiteshP/nvim-navic",
     lazy = true,
@@ -136,45 +73,12 @@ return {
         end
       end)
     end,
-    opts = function()
-      return {
-        separator = " ❯ ",
-        highlight = true,
-        depth_limit = 5,
-      }
-    end,
-  },
-
-  -- TODO: look up if this needs more configuration
-  -- { "stevearc/dressing.nvim", config = true },
-
-  -- TODO, simplify this by only using navic?
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    event = "VeryLazy",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
+    opts = {
+      separator = " ❯ ",
+      highlight = true,
+      depth_limit = 5,
     },
-    config = function()
-      require("barbecue").setup({
-        attach_navic = false,
-        create_autocmd = false,
-        -- show_dirname = false,
-      })
-
-      vim.api.nvim_create_autocmd({
-        "CursorHold",
-        "InsertLeave",
-      }, {
-        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-        callback = function()
-          require("barbecue.ui").update()
-        end,
-      })
-    end,
   },
+
   { "j-hui/fidget.nvim", event = "VeryLazy", config = true },
 }
