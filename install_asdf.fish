4d87f1dac4f124a67c6e6724801c7bba8f38d97f
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2023-10-20 13:25:23 -0400

    Update some scripts

--- install_asdf.fish
+++ install_asdf.fish
@@ -4,7 +4,7 @@
 ./install
 set -Ux ASDF_DIR ~/.local/opt/asdf
 set -Ux ASDF_DATA_DIR ~/.local/opt/asdf_installs
-git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch v0.11.1
+git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch v0.13.1
 fish_add_path $ASDF_DIR/bin
 for PLUGIN in (cut -d ' ' -f1 ~/.tool-versions)
     eval asdf plugin add $PLUGIN
