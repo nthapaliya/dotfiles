94d599184374141b3080c01a248051b0840f17f1
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2024-08-16 01:16:21 -0400

    More config

---
+++ home/.config/wezterm/helpers.lua
@@ -0,0 +1,83 @@
+local wez = require("wezterm")
+
+local module = {}
+
+RSEP = wez.nerdfonts.ple_right_half_circle_thick
+LSEP = wez.nerdfonts.ple_left_half_circle_thick
+DOT = wez.nerdfonts.oct_dot_fill
+
+local function insert_text(tbl, data)
+	table.insert(tbl, { Attribute = { Intensity = data.intensity or "Normal" } })
+	table.insert(tbl, { Background = { Color = data.bg_color } })
+	table.insert(tbl, { Foreground = { Color = data.fg_color } })
+	table.insert(tbl, { Text = " " .. data.text .. " " })
+end
+
+local function insert_lsep(tbl, data)
+	table.insert(tbl, { Background = { Color = data.bar_bg } })
+	table.insert(tbl, { Foreground = { Color = data.bg_color } })
+	table.insert(tbl, { Text = data.lsep or LSEP })
+end
+
+local function insert_rsep(tbl, data)
+	table.insert(tbl, { Background = { Color = data.bar_bg } })
+	table.insert(tbl, { Foreground = { Color = data.bg_color } })
+	if data.rsep then
+		table.insert(tbl, { Text = data.rsep })
+	else
+		table.insert(tbl, { Text = RSEP .. " " }) -- this space is BETWEEN segments. keep
+	end
+end
+
+function module.basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+function module.short_cwd(str)
+	str = string.gsub(str, wez.home_dir, "~")
+	str = string.gsub(str, "([^/])[^/]+/", "%1/")
+
+	return str
+end
+
+function module.tab_title(tab)
+	local name = tab.active_pane.foreground_process_name
+	local name_clean
+	if #name == 0 then
+		name_clean = string.gsub(tab.active_pane.title, "(%w)( .+)", "%1")
+	else
+		name_clean = module.basename(name)
+	end
+
+	return tostring(tab.tab_index + 1) .. " | " .. name_clean
+end
+
+function module.segment(segment)
+	local s = {}
+
+	insert_lsep(s, segment)
+	insert_text(s, segment)
+	insert_rsep(s, segment)
+
+	return wez.format(s)
+end
+
+function module.segments(segments)
+	local s = {}
+
+	for i, segment in ipairs(segments) do
+		if i == 1 then
+			insert_lsep(s, segment)
+		end
+
+		insert_text(s, segment)
+
+		if i == #segments then
+			insert_rsep(s, segment)
+		end
+	end
+
+	return wez.format(s)
+end
+
+return module
--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -12,171 +12,105 @@
 -- background even when the window is closed)
 
 local wez = require("wezterm")
-local act = wez.action
+local helpers = require("helpers")
 
 local config = wez.config_builder()
 
+local font = { family = "JetBrains Mono" }
+
 config.color_scheme = "Catppuccin Mocha"
-config.font = wez.font({ family = "JetBrains Mono", weight = "Light" })
+config.font = wez.font(font)
 config.font_size = 14.0
 config.tab_max_width = 32
-config.scrollback_lines = 10000000
-
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 20
+config.scrollback_lines = 10000
 
 config.native_macos_fullscreen_mode = true
 config.window_close_confirmation = "AlwaysPrompt"
 config.window_decorations = "RESIZE"
 
--- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
--- config.window_padding = {
--- 	left = "0.5cell",
--- 	right = "0.5cell",
--- 	top = "1.5cell",
--- 	bottom = "0.5cell",
--- }
-
 config.set_environment_variables = {
 	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
 }
 
--- keys configuration
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-	{
-		key = ",",
-		mods = "SUPER",
-		action = act.SpawnCommandInNewTab({
-			cwd = wez.home_dir,
-			args = { "nvim", wez.config_file },
-		}),
-	},
-	{
-		mods = "LEADER",
-		key = '"',
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "%",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-
-	{ key = "Enter", mods = "LEADER", action = act.ActivateCopyMode },
-	-- { key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	-- { key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{ key = "p", mods = "LEADER|CTRL", action = act.ActivateTabRelative(-1) },
-	{ key = "n", mods = "LEADER|CTRL", action = act.ActivateTabRelative(1) },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-
-	{
-		key = "f",
-		mods = "CMD|SHIFT",
-		action = act.ToggleFullScreen,
-	},
-
-	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollToPrompt(-1) },
-	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
-}
-
-for i = 1, 9 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i - 1),
-	})
-end
-
--- smart splits config
-local smart_splits = wez.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-smart_splits.apply_to_config(config)
+require("keys").apply_to_config(config)
 
 -- tab_bar configuration
 config.show_new_tab_button_in_tab_bar = false -- keep
 config.tab_bar_at_bottom = true
--- config.show_close_tab_button_in_tabs = false
+-- config.show_close_tab_button_in_tabs = false -- not valid yet
 config.use_fancy_tab_bar = false
 
-local darker_grey = "#2e2e2e"
-local lighter_grey = "#575757"
-
-local offwhite = "#dddddd"
-local blue = "#0087af"
-local white = "#ffffff"
-local black = "#000000"
+local darker_grey = "#2e2e2e" -- tab bar background
+local lighter_grey = "#575757" -- inactive tab bar color
+local offwhite = "#dddddd" -- inactive tab bar text color
+local blue = "#0087af" -- active tab bar color
+local white = "#ffffff" -- active tab bar text color
+local black = "#000000" -- left and right sidebar text (on offwhite background)
 
 config.window_frame = {
-	font = wez.font({ family = "JetBrains Mono", weight = "Bold" }),
-	font_size = 14.0,
+	font = wez.font(font),
+	font_size = 13.0,
 	active_titlebar_bg = darker_grey,
 	inactive_titlebar_bg = darker_grey,
 }
-config.colors = {
-	tab_bar = {
-		inactive_tab_edge = lighter_grey,
-	},
-}
+
 config.colors = {
 	tab_bar = {
 		inactive_tab_edge = lighter_grey,
 		background = darker_grey,
-		active_tab = {
-			bg_color = blue,
-			fg_color = white,
-			intensity = "Bold",
-		},
-		inactive_tab = {
-			bg_color = lighter_grey,
-			fg_color = offwhite,
-			intensity = "Half",
-		},
 	},
 }
 
--- local function basename(s)
--- 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
--- end
-
 wez.on("update-status", function(window, pane)
-	local domain_name = pane:get_domain_name()
-	local time = wez.time.now():format(" %Y-%m-%d | %H:%M ")
+	window:set_left_status(helpers.segment({
+		text = pane:get_domain_name(),
+		bg_color = offwhite,
+		fg_color = black,
+		intensity = "Bold",
+		bar_bg = darker_grey,
+	}))
 
+	local segments = {}
+	local time = wez.time.now():format("%Y-%m-%d | %H:%M")
 	local cwd = pane:get_current_working_dir()
 
-	local cwd_path = cwd and (" " .. string.gsub(cwd.file_path, wez.home_dir, "~") .. " |") or ""
-	local hostname = cwd and cwd.host or wez.hostname()
-
-	window:set_left_status(wez.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Background = { Color = offwhite } },
-		{ Foreground = { Color = black } },
-		{ Text = " " .. domain_name .. " " },
-	}))
-
-	window:set_right_status(wez.format({
-		-- { Attribute = { Intensity = "Normal" } },
-		{ Background = { Color = lighter_grey } },
-		{ Foreground = { Color = offwhite } },
-		{ Text = cwd_path },
-		{ Text = time },
-		{ Background = { Color = offwhite } },
-		{ Foreground = { Color = black } },
-		{ Text = " " .. hostname .. " " },
-	}))
-end)
-
-wez.on("format-tab-title", function(tab)
-	local name = tab.active_pane.foreground_process_name
-	local basename
-	if #name == 0 then
-		basename = string.gsub(tab.active_pane.title, "(%w)( .+)", "%1")
-	else
-		basename = string.gsub(name, "(.*[/\\])(.*)", "%2")
+	if cwd then
+		local path = helpers.short_cwd(cwd.file_path)
+		table.insert(segments, { text = path, bg_color = lighter_grey, fg_color = white, bar_bg = darker_grey })
 	end
 
-	return " " .. (tab.tab_index + 1) .. " | " .. basename .. " "
+	table.insert(segments, { text = time, bg_color = lighter_grey, fg_color = offwhite, bar_bg = darker_grey })
+
+	if cwd and cwd.host and #cwd.host > 0 then
+		table.insert(segments, { text = cwd.host, bg_color = offwhite, fg_color = black, bar_bg = darker_grey })
+	end
+
+	window:set_right_status(helpers.segments(segments))
+end)
+
+wez.on("format-tab-title", function(tab, _, _, localconfig, hover)
+	local segment = {
+		text = helpers.tab_title(tab),
+		bg_color = lighter_grey,
+		fg_color = offwhite,
+		intensity = "Half",
+		bar_bg = darker_grey,
+	}
+
+	if localconfig.use_fancy_tab_bar then
+		segment.lsep = ""
+		segment.rsep = ""
+	end
+
+	if tab.is_active then
+		segment.bg_color = blue
+		segment.fg_color = white
+		segment.intensity = "Bold"
+	elseif hover then
+		segment.intensity = "Bold"
+	end
+
+	return helpers.segment(segment)
 end)
 
 return config
