1137a3567166be70df59b61f1e64240e71692579
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2026-02-09 17:57:56 -0500

    Clean up fish stuff

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,12 +1,11 @@
 # set_uvars
+# fish_config theme choose coolbeans
 set_abbrs
 
 if command -sq atuin
     # atuin init fish --disable-up-arrow --disable-ctrl-r | source
     atuin init fish --disable-up-arrow | source
 end
-
-# set -gx NODE_OPTIONS --openssl-legacy-provider
 
 if command -sq fzf
     fzf --fish | source
@@ -19,10 +18,6 @@ end
 if command -sq zoxide
     eval "$(zoxide init fish --cmd cd)"
 end
-
-fish_config theme choose coolbeans
-
-set -gx SHELL fish
 
 if command -sq nvim
     set -gx EDITOR (which nvim)
@@ -42,3 +37,15 @@ end
 if command -sq fd
     set -gx FZF_ALT_C_COMMAND 'fd -H --type directory'
 end
+
+if test Darwin = (uname)
+    fish_add_path -a /opt/homebrew/bin
+
+    set -gx XDG_CACHE_HOME ~/Library/Caches/xdg-cache
+    set -gx HOMEBREW_BUNDLE_FILE ~/.config/brew/Brewfile
+end
+
+fish_add_path ~/.local/bin
+set -e fish_greeting
+set -gx SHELL fish
+# set -gx NODE_OPTIONS --openssl-legacy-provider
