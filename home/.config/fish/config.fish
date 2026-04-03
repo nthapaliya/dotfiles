fcf2ea5857dc4cf828993f29c42ccd4402d28880
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2026-04-03 19:16:25 -0400

    config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,47 +1,49 @@
 # Essentials:
-
 # fish_config theme choose coolbeans
 fish_add_path ~/.local/bin
 set -U fish_greeting
 set -gx SHELL fish
-set_abbrs
+
+## nvim
+set -gx EDITOR (which nvim)
+set -gx VISUAL $EDITOR
+set -gx MANPAGER "nvim +Man!"
+
+# abbrs:
+## git
+abbr -a -- g git
+abbr -a -- gb 'git branch'
+abbr -a -- gcm 'git checkout (git symbolic-ref --short refs/remotes/origin/HEAD | cut -d / -f 2)'
+abbr -a -- gco 'git checkout'
+abbr -a -- gdca 'git diff --cached'
+abbr -a -- gs 'git status -s  | cut -c4-'
+abbr -a -- gss 'git status -s'
+abbr -a -- vconflict 'vi (git conflict)'
+abbr -a -- vdirty 'vi ( git status -s | cut -c4- )'
+
+## dotfiles
+abbr -a -- d 'cd ~/Projects/dotfiles'
+abbr -a -- htree 'tree -a -I plugged\|\.git'
+abbr -a -- lsd 'ls -d .*'
+abbr -a -- p 'cd ~/Projects'
+abbr -a -- e 'cd ~/Projects/euler'
 
 # Command specific:
+## atuin
 if command -sq atuin
     # atuin init fish --disable-up-arrow --disable-ctrl-r | source
     atuin init fish --disable-up-arrow | source
 end
 
-if command -sq nvim
-    set -gx EDITOR (which nvim)
-    set -gx VISUAL $EDITOR
-    set -gx MANPAGER "nvim +Man!"
-else
-    set -gx EDITOR (which vim)
-    set -gx VISUAL $EDITOR
-end
-
-if command -sq fzf
-    if command -sq rg
-        set -gx RIPGREP_CONFIG_PATH ~/.config/rg/config
-        set -gx FZF_DEFAULT_COMMAND 'rg --files'
-    end
-
-    if command -sq fd
-        set -gx FZF_ALT_C_COMMAND 'fd -H --type directory'
-        set -gx FZF_DEFAULT_COMMAND 'fd -H --type f'
-    end
-
-    set -gx FZF_CTRL_T_OPTS "
+## [fzf, rg, fd, bat, tmux]: assume available
+set -gx RIPGREP_CONFIG_PATH ~/.config/rg/config
+set -gx FZF_DEFAULT_COMMAND 'fd -H --type f'
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -gx FZF_CTRL_T_OPTS "
         --tmux 90%,50% \
         --preview 'bat -n --color=always {}' \
         --bind 'ctrl-/:change-preview-window(down|hidden|)' "
-
-    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-    # set -gx FZF_DEFAULT_OPTS --tmux
-
-    fzf --fish | source
-end
+fzf --fish | source
 
 # OS Specific
 if test Darwin = (uname)
