7f6d210cd97c03468ab6d449197bf0c88593509d
Niraj Thapaliya <nthapaliya@users.noreply.github.com>
2026-04-25 13:45:29 -0400

    Some updates

--- home/.config/fish/functions/jupyter.fish
+++
@@ -1,8 +0,0 @@
-function jupyter
-    # General instructions
-    # uv venv ~/Projects/venvs/jupyter
-    # cd ~/Projects/venvs/jupyter
-    # uv pip install jupyterlab jupyter-ruff jupyterlab-vim ipykernel python-localvenv-kernel
-
-    ~/Projects/venvs/jupyter/bin/jupyter $argv
-end
